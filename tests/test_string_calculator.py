c41fe19fa0627f62c54b91074b35cfa58d9dc7fc
Nilkesh Srivastava <65970248+Nilkesh95@users.noreply.github.com>
2025-01-29 18:11:39 +0000

    Enhancement: Support any amount of numbers

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,8 +1,14 @@
 import pytest
 from string_calculator import add
 
+#Empty string returns 0 test case
 def test_empty_string():
     assert add("")==0
 
+#Test case for two numbers
 def test_two_numbers():
     assert add("1,2")==3
+
+#Test case for handling multiple numbers
+def test_multiple_numbers():
+    assert add("1,2,3,4")==10
